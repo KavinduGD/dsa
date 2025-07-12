c83d5fe1acc3cc7bdf1e7749366d158df616e377
KavinduGD <kavidudharmasiri90@gmail.com>
2025-07-12 21:48:35 +0530

    frequency counter q   solved

--- leetcode/array/01_Array_Partistion.py
+++ leetcode/array/01_Array_Partistion.py
@@ -11,7 +11,7 @@ class Solution(object):
                         arr[j] = arr[j+1]
                         arr[j+1] = temp
 
-        sorted_arr = sort_arr(nums)
+        sort_arr(nums)
 
         total = 0
 
@@ -29,3 +29,14 @@ print(solution.arrayPairSum([1, 3, 4, 2]))
 
 
 # https://leetcode.com/problems/array-partition
+
+# class Solution(object):
+#     def arrayPairSum(self, nums):
+#         nums.sort()  # Fast built-in sort: O(n log n)
+#         total = 0
+#         for i in range(0, len(nums), 2):
+#             total += nums[i]
+#         return total
+
+# solution = Solution()
+# print(solution.arrayPairSum([1, 3, 4, 2]))  # Output: 4 ✅
---
+++ practise_questions/frequencyCounter.js
@@ -0,0 +1,71 @@
+/*
+Write a function called same, which accepts two arrays.
+The function should return true if every value in the array has its corresponding value squared in the second array.
+The frequency of values must be the same.
+
+same([1, 2, 3], [4, 1, 9]) // true  
+same([1, 2, 3], [1, 9])    // false  
+same([1, 2, 1], [4, 4, 1]) // false (must be same frequency)
+
+*/
+
+//input - arr1, arr2
+
+//one element from 1st arr as 1 corresponding  square  element
+// after checking that should remove compare that again
+
+/*for loop arr1
+    i ^2
+    for arr 2
+        compare if has add -1
+
+O(n^2)
+*/
+
+/*
+check 2 arr length
+for loop
+take object with frq
+compare obj
+
+O(n)
+*/
+
+function check(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  let frq1 = {};
+  let frq2 = {};
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (frq1[arr1[i] ** 2]) {
+      frq1[arr1[i] ** 2]++;
+    } else {
+      frq1[arr1[i] ** 2] = 1;
+    }
+
+    if (frq2[arr2[i]]) {
+      frq2[arr2[i]]++;
+    } else {
+      frq2[arr2[i]] = 1;
+    }
+  }
+  //   console.log(frq1);
+  //   console.log(frq2);
+
+  for (let key of Object.keys(frq1)) {
+    if (frq1[key] !== frq2[key]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+let arr1 = [1, 2, 3, 5, 7];
+let arr2 = [1, 2, 3, 6, 8];
+
+console.log(check([1, 2, 3], [1, 9])); // false
+console.log(check([1, 2, 1], [4, 4, 1])); // false (must be same frequency)
+console.log(check([1, 2, 3], [4, 1, 9])); // true
