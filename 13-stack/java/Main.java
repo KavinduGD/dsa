b239afac29654feab49019285dd0ef874f9e5c51
Kavindu Gihan <kavidudharmasiri90@gmail.com>
2026-04-18 22:11:55 +0530

    stack java update

--- 13-stack/java/Main.java
+++ 13-stack/java/Main.java
@@ -1,9 +1,14 @@
 public class Main{
     public static void main(String[] args) {
         double result;
-        Stackx stack= new Stackx(3);
+        Stackx stack= new Stackx(6);
         
         stack.push(8);
+        stack.push(3);
+        stack.push(5);
+        stack.push(6);
+        stack.push(12);
+        stack.push(34);
         System.err.println(stack.peak());
 
         result= stack.pop();
